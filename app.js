33d38a286167e2398190298d2f8736a0d4479e01
johnbabu007 <55944699+johnbabu007@users.noreply.github.com>
2021-06-17 16:07:54 +1000

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! I am JOhn'));
+app.get('/', (req, res) => res.send('This is sample file written by john'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
